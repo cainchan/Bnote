fc2e9dd0c2b674c6298729a02c33e916dd672708
kaychen <kaychen.cn@gmail.com>
2017-03-13 23:23:20 +0800

    login+reg page

--- Server/application/controllers/User.php
+++ Server/application/controllers/User.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Welcome extends CI_Controller {
+class User extends CI_Controller {
 
 	/**
 	 * Index Page for this controller.
@@ -20,7 +20,14 @@ class Welcome extends CI_Controller {
 	 */
 	public function index()
 	{
-		$this->load->database();
 		$this->load->view('welcome_message');
+	}
+	public function login()
+	{
+		$this->load->view('webapp/login');
+	}
+	public function reg()
+	{
+		$this->load->view('webapp/register');
 	}
 }
---
+++ Server/application/views/webapp/login.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="zh-CN">
+<head>
+	<meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Index</title>
+	<!-- 最新版本的 Bootstrap 核心 CSS 文件 -->
+<link rel="stylesheet" href="https://cdn.bootcss.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://cdn.bootcss.com/highlight.js/9.9.0/styles/default.min.css">
+<link rel="stylesheet" href="https://cdn.bootcss.com/jquery-contextmenu/2.4.3/jquery.contextMenu.min.css">
+<link rel="stylesheet" href="/public/css/style.css">
+</head>
+<body>
+<div id="web" class="text-center">
+	<p><h3>蜜蜂笔记</h3></p>
+	<div class="login form-horizontal">
+	  <div class="form-group">
+	    <label for="inputEmail3" class="col-sm-3 control-label">邮箱</label>
+	    <div class="col-sm-9">
+	      <input type="email" class="form-control" placeholder="邮箱">
+	    </div>
+	  </div>
+	  <div class="form-group">
+	    <label for="inputPassword3" class="col-sm-3 control-label">密码</label>
+	    <div class="col-sm-9">
+	      <input type="password" class="form-control" placeholder="密码">
+	    </div>
+	  </div>
+	  <a type="button" class="btn btn-default">登录</a>
+	  <a type="button" href="/user/reg" class="btn btn-default">注册</a>
+	</div>
+</div>
+</body>
+</html>
