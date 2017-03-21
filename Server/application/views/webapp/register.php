4a68435f3e324a7ca43f1922ff19e14019868245
KayChen <kaychen.cn@gmail.com>
2017-03-21 17:59:10 +0800

    fix title

--- Server/application/views/webapp/register.php
+++ Server/application/views/webapp/register.php
@@ -4,7 +4,7 @@
 	<meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>注册</title>
+	<title>注册 - 蜜蜂笔记 - BNote</title>
 	<!-- 最新版本的 Bootstrap 核心 CSS 文件 -->
 <link rel="stylesheet" href="https://cdn.bootcss.com/bootstrap/3.3.7/css/bootstrap.min.css">
 <link rel="stylesheet" href="https://cdn.bootcss.com/highlight.js/9.9.0/styles/default.min.css">
